7472f6eafe0a9944f20b789156480d17b63abbf0
Ankush kumar <91752463+Ankushpatna78@users.noreply.github.com>
2023-07-12 23:14:44 +0530

    Add files via upload

---
+++ Lecture 19.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	vector<string> solar_sys={"mer","venus","earth","mars","jupitor","satrun","uranus","neptune"};
+	vector<string>::iterator shivam=solar_sys.begin();
+	vector<string>::iterator champa=solar_sys,begin();
+	return 0;
+}
